a474d1ca98413ad55f8bbf68e6707863f4bf19cc
at-robins <30956028+at-robins@users.noreply.github.com>
2024-02-11 00:12:09 +0100

    Changed graph representation
    The representation of the clustering resolution graph has been changed.

--- src/graph.rs
+++ src/graph.rs
@@ -1,11 +1,12 @@
-use std::{collections::HashMap, hash::Hash, rc::Rc};
+use std::{
+    borrow::Borrow,
+    collections::HashMap,
+    rc::Rc,
+};
 
 use getset::{CopyGetters, Getters};
-use ordered_float::OrderedFloat;
 
 use crate::data::{ClusterStabilityData, ResolutionData};
-
-const FAKE_NODE_STABILITY: OrderedFloat<f64> = OrderedFloat(1.0);
 
 /// Aggregates the [`ResolutionData`] vector by number of clusters present.
 ///
@@ -27,153 +28,140 @@ pub fn aggregate_by_number_of_clusters(
     cluster_map
 }
 
-/// Returns the root node of a cluster stability tree sampled at different resolutions.
+/// Returns the root nodes of a cluster stability graph sampled at different resolutions
+/// and ordered in layers depending on the respective number of clusters.
 ///
 /// # Parameters
 ///
-/// * `resolutions` - the resolution data to build the tree from
-pub fn to_tree(resolutions: Vec<ResolutionData>) -> Rc<ResolutionNode> {
+/// * `resolutions` - the resolution data to build the graph from
+pub fn to_graph(resolutions: Vec<ResolutionData>) -> Vec<Rc<ResolutionNode>> {
     let map = aggregate_by_number_of_clusters(resolutions);
     let mut ordered_cluster_keys: Vec<usize> = map.keys().cloned().collect();
     ordered_cluster_keys.sort();
-    // Starts with the final node that has a fake resolution, which is never used.
-    let mut successor_nodes: Vec<Rc<ResolutionNode>> =
-        vec![Rc::new(ResolutionNode::create_end_node())];
+
+    // Returns an empty vector if there are no clusters.
+    if ordered_cluster_keys.is_empty() {
+        return Vec::new();
+    }
+
+    let mut potential_parent_nodes: Vec<Rc<ResolutionNode>> = Vec::new();
     let mut previous_cluster_key: Option<usize> = None;
-    for (i, cluster_key) in ordered_cluster_keys.into_iter().rev().enumerate() {
+    for (i, cluster_key) in ordered_cluster_keys.into_iter().enumerate() {
         let resolutions = map.get(&cluster_key).expect(
             "The key was obtained directly from the map so there must be an associated value.",
         );
-        successor_nodes = if i == 0 {
-            // The first element / the highest cluster number.
+        potential_parent_nodes = if i == 0 {
+            // The first cluster elements do not have parent nodes.
             resolutions
                 .iter()
-                .map(|resolution| {
-                    Rc::new(ResolutionNode::create_intermediate_node(
-                        resolution.resolution(),
-                        successor_nodes
-                            .iter()
-                            // The distance to the fake end node is set to the minimum distance value / maximum stability.
-                            .map(|end_node| (Rc::clone(end_node), FAKE_NODE_STABILITY))
-                            .collect(),
-                    ))
-                })
+                .map(|resolution| Rc::new(ResolutionNode::new(resolution.resolution())))
                 .collect()
         } else {
-            // Intermediate elements.
+            // Other cluster elements have parents and according stabilities.
             let previous_resolutions = map.get(
-                previous_cluster_key
-                    .as_ref()
-                    .expect("The previous key must have been set on a previous iteration."),
-            )
-            .expect(
-                "The key was obtained directly from the map so there must be an associated value.",
-            );
+                            previous_cluster_key
+                                .as_ref()
+                                .expect("The previous key must have been set on a previous iteration."),
+                        )
+                        .expect(
+                            "The key was obtained directly from the map so there must be an associated value.",
+                        );
             resolutions
                 .iter()
                 .map(|resolution| {
-                    Rc::new(ResolutionNode::create_intermediate_node(
-                        resolution.resolution(),
-                        successor_nodes
-                        .iter()
-                        .enumerate()
-                        // The distance to the fake end node is set to the minimum value.
-                            .map(|(i, node)| {
-                                let stability_data = ClusterStabilityData::from_clustering(resolution, &previous_resolutions[i]).expect("The number of clusters cannot be equal as sorting happend beforehand.");
-                                (Rc::clone(node), OrderedFloat(stability_data.mean_stability()))
-                            })
-                            .collect(),
+                    let mut optimal_node: Option<ResolutionNode> = None;
+                    for (i, potential_parent_node) in potential_parent_nodes.iter().enumerate() {
+                        let stability_data = ClusterStabilityData::from_clustering(
+                            resolution,
+                            &previous_resolutions[i],
+                        )
+                        .expect(
+                            "The number of clusters cannot be equal as sorting happend beforehand.",
+                        );
+                        let potential_child_node = ResolutionNode::new_with_parent(
+                            resolution.resolution(),
+                            potential_parent_node,
+                            stability_data.mean_stability(),
+                        );
+                        // The optimal node has the highest overall stability and resolution.
+                        // Defaults to true if unset so that the optimal node gets set on the first iteration.
+                        if optimal_node.as_ref().map_or(true, |current_optimal_node| {
+                            potential_child_node.total_stability()
+                                > current_optimal_node.total_stability()
+                                || (potential_child_node.total_stability()
+                                    == current_optimal_node.total_stability())
+                                    && potential_child_node.resolution()
+                                        > current_optimal_node.resolution()
+                        }) {
+                            optimal_node = Some(potential_child_node)
+                        }
+                    }
+                    Rc::new(optimal_node.expect(
+                        "This must be set as there cannot be empty parent clustering data.",
                     ))
                 })
                 .collect()
         };
         previous_cluster_key = Some(cluster_key);
     }
-    // Returns a fake starting node with minimal distance / maximal stability to all other nodes with the lowest cluster number.
-    Rc::new(ResolutionNode::create_start_node(
-        successor_nodes
-            .iter()
-            .map(|node| (Rc::clone(node), FAKE_NODE_STABILITY))
-            .collect(),
-    ))
+    // Returns the optimal leaf nodes.
+    potential_parent_nodes
 }
 
-/// Returns the path that optimally conserves stability between clusters in a tree of cluster [`ResolutionData`].
-/// 
-/// # Parameters
-///
-/// * `resolutions` - the resolution data to calulate the optimal stability path for
-pub fn optimal_stability_path(resolutions: Vec<ResolutionData>) -> Option<(Vec<Rc<ResolutionNode>>, OrderedFloat<f64>)> {
-    println!("Creating cluster stability tree.");
-    let root = to_tree(resolutions);
-    println!("Calculating shortest path.");
-    pathfinding::directed::dijkstra::dijkstra(
-        &root,
-        |node| node.successors_with_distance(),
-        |node| node.is_final_node(),
-    )
-}
-
-#[derive(CopyGetters, Getters, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone)]
+#[derive(CopyGetters, Getters, Debug, PartialEq, PartialOrd, Clone)]
 /// A node in a connected resolution graph, where edges are defined as cluster stability between nodes.
 pub struct ResolutionNode {
     /// The resolution of the node if applicable.
     /// Starting and end node do not have a resolution as they are fake nodes.
     #[getset(get_copy = "pub")]
-    resolution: Option<OrderedFloat<f64>>,
-    /// The nodes and stabilities connected to this node.
+    resolution: f64,
+    /// The optimal parent node and the according cluster stability.
     #[getset(get = "pub")]
     // There will be no cycles so using a simple Rc is not producing memory leaks.
-    successors: Vec<(Rc<ResolutionNode>, OrderedFloat<f64>)>,
+    optimal_parent: Option<Rc<ResolutionNode>>,
+    /// The cluster stability of the optimal parent-child-transition.
+    #[getset(get_copy = "pub")]
+    optimal_stability: Option<f64>,
+    /// The sum of optimal stability transitions needed to reach this node from the root node.
+    #[getset(get_copy = "pub")]
+    total_stability: f64,
 }
 
 impl ResolutionNode {
-    /// Returns the successor nodes with the according distance, which is inversly proportional to the cluster stability between the two nodes.
-    pub fn successors_with_distance(&self) -> Vec<(Rc<ResolutionNode>, OrderedFloat<f64>)> {
-        self.successors
-            .iter()
-            .map(|(resolution, stability)| (Rc::clone(resolution), OrderedFloat(1.0) - stability))
-            .collect()
-    }
-
-    /// Returns ```true``` if the node is an end / leaf node.
-    pub fn is_final_node(&self) -> bool {
-        self.successors.is_empty()
-    }
-
-    /// Creates an end / leaf node.
-    fn create_end_node() -> Self {
-        Self {
-            resolution: None,
-            successors: Vec::new(),
-        }
-    }
-
-    /// Creates a start / root node with the specified successor nodes.
+    /// Creates a new root node.
     ///
     /// # Parameters
     ///
-    /// * `successors` - the connected child nodes
-    fn create_start_node(successors: Vec<(Rc<ResolutionNode>, OrderedFloat<f64>)>) -> Self {
+    /// * `resolution` - the resolution of the node
+    pub fn new(resolution: f64) -> Self {
         Self {
-            resolution: None,
-            successors,
+            resolution,
+            optimal_parent: None,
+            optimal_stability: None,
+            total_stability: 0.0,
         }
     }
 
-    /// Creates an intermediate node with the specified resolution and successor nodes.
+    /// Creates a new child node.
     ///
     /// # Parameters
     ///
-    /// * `resolution` - the resolution of the clustering the node corresponds to
-    /// * `successors` - the connected child nodes
-    fn create_intermediate_node(
+    /// * `resolution` - the resolution of the node
+    /// * `optimal_parent` - stability-wise the optimal parent node for this child node
+    /// * `optimal_stability` - the cluster stability of the optimal parent-child-transition.
+    /// including the stability for the transition of parent to child
+    pub fn new_with_parent<T: Borrow<Rc<ResolutionNode>>>(
         resolution: f64,
-        successors: Vec<(Rc<ResolutionNode>, OrderedFloat<f64>)>,
+        optimal_parent: T,
+        optimal_stability: f64,
     ) -> Self {
+        let optimal_parent = Rc::clone(optimal_parent.borrow());
+        let total_stability = optimal_parent.total_stability() + optimal_stability;
         Self {
-            resolution: Some(resolution.into()),
-            successors,
+            resolution,
+            optimal_parent: Some(optimal_parent),
+            optimal_stability: Some(optimal_stability),
+            total_stability,
         }
     }
 }
